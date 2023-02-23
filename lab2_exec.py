e6fda22cdf98a141736499b0ab8a810b0ee0abb9
Mr.Нет <42790040+xiongdawei@users.noreply.github.com>
2023-02-23 09:24:20 -0600

    Add files via upload

--- lab2_exec.py
+++ lab2_exec.py
@@ -49,18 +49,21 @@ TODO: Initialize Q matrix
 
 Q11 = [145.45*pi/180, -54.18*pi/180, 120.78*pi/180, -162.75*pi/180, -89.84*pi/180, (-67.46)*pi/180] # Left Tower Bottom)
 Q12 = [144.54*pi/180, -61.88*pi/180, 120.63*pi/180, -154.91*pi/180, -89.67*pi/180, (-68.44)*pi/180] # Left Tower Middle
-Q13 = [144.57*pi/180, -67.95*pi/180, 119.05*pi/180, -147.26*pi/180, -89.62*pi/180, (-68.51)*pi/180] # Left Tower Top
+Q13 = [145.66*pi/180, -69.75*pi/180, 118.22*pi/180, -144.35*pi/180, -89.62*pi/180, (-68.51)*pi/180] # Left Tower Top
+T1 = [145.66*pi/180, -73.77*pi/180, 116.29*pi/180, -138.67*pi/180, -89.62*pi/180, (-68.59)*pi/180]  # Left Tower High Point
 Q21 = [165.32*pi/180, -58.08*pi/180, 128.27*pi/180, -164.43*pi/180, -94.21*pi/180, (-25.25)*pi/180] # Middle Tower Bottom
 Q22 = [166.36*pi/180, -63.79*pi/180, 122.63*pi/180, -154.42*pi/180, -91.97*pi/180, (-46.73)*pi/180] # Middle Tower Middle
 Q23 = [165.82*pi/180, -71.24*pi/180, 121.80*pi/180, -146.01*pi/180, -91.09*pi/180, (-47.65)*pi/180] # Middle Tower Top
+T2 = [165.85*pi/180, -75.94*pi/180, 119.49*pi/180, -139.00*pi/180, -91.05*pi/180, (-47.71)*pi/180]  # Middle Tower High Point
 Q31 = [189.23*pi/180, -51.26*pi/180, 110.83*pi/180, -153.69*pi/180, -93.93*pi/180, (-23.90)*pi/180] # Right Tower Bottom
 Q32 = [189.24*pi/180, -57.71*pi/180, 110.07*pi/180, -146.49*pi/180, -93.89*pi/180, (-23.97)*pi/180] # Right Tower Middle
 Q33 = [189.26*pi/180, -63.80*pi/180, 108.07*pi/180, -138.41*pi/180, -93.84*pi/180, (-24.05)*pi/180] # Right Tower Top
+T3 = [189.26*pi/180, -68.46*pi/180, 107.85*pi/180, -133.69*pi/180, -93.05*pi/180, (-24.39)*pi/180]  # Right Tower High Point
 
 # Initialize Q Matrix
-Q = [[Q11, Q12, Q13],
-     [Q21, Q22, Q23],
-     [Q31, Q32, Q33]]
+Q = [[Q11, Q12, Q13, T1],
+     [Q21, Q22, Q23, T2],
+     [Q31, Q32, Q33, T3]]
 ############### Your Code End Here ###############
 
 ############## Your Code Start Here ##############
@@ -74,6 +77,12 @@ def gripper_callback(msg):
     global digital_in_0
 
     digital_in_0 = msg.DIGIN
+
+    global analog_in_0
+    global analog_in_1
+
+    analog_in_0 = msg.AIN0
+    analog_in_1 = msg.AIN1
 
 
 
@@ -198,33 +207,37 @@ def move_arm(pub_cmd, loop_rate, dest, vel, accel):
 
 def move_block(pub_cmd, loop_rate, start_loc, start_height,
                end_loc, end_height):
-    move_arm(pub_cmd, loop_rate, Q[start_loc][2], 4.0, 4.0)
+    move_arm(pub_cmd, loop_rate, Q[start_loc][3], 4.0, 4.0)
     move_arm(pub_cmd, loop_rate, Q[start_loc][start_height], 4.0, 4.0)
     gripper(pub_cmd, loop_rate, suction_on)
     time.sleep(0.5)
-    move_arm(pub_cmd. loop_rate, Q[start_loc][2], 4.0,4.0)
-    move_arm(pub_cmd, loop_rate, Q[end_loc][2], 4.0, 4.0)
-    move_arm(pub_cmd, loop_rate, Q[end_loc][end_height], 4.0, 4.0)
-    gripper(pub_cmd, loop_rate, suction_off)
-    time.sleep(0.5)
-    move_arm(pub_cmd, loop_rate, Q[end_loc][2], 4.0, 4.0)
+    if(digital_in_0 == 0):
+        gripper(pub_cmd, loop_rate, suction_off)
+        error = 1
+    else:
+        move_arm(pub_cmd, loop_rate, Q[start_loc][3], 4.0,4.0)
+        move_arm(pub_cmd, loop_rate, Q[end_loc][3], 4.0, 4.0)
+        move_arm(pub_cmd, loop_rate, Q[end_loc][end_height], 4.0, 4.0)
+        gripper(pub_cmd, loop_rate, suction_off)
+        time.sleep(0.5)
+        move_arm(pub_cmd, loop_rate, Q[end_loc][3], 4.0, 4.0)
+        error = 0
 
-    error = 0
     return error
 
 
 ############### Your Code End Here ###############
 
 
-def TowerOfHanoi(n, start_loc, end_loc, aux_rod, pub_cmd, loop_rate, height):
-    if n == 0:
-        return
-    TowerOfHanoi(n-1, start_loc, aux_rod, end_loc, pub_cmd, loop_rate, height)
-    move_block(pub_cmd, loop_rate, start_loc, height[start_loc], end_loc, height[end_loc])
-    print("Move disk", n, "from rod", start_loc, "to rod", end_loc)
-    height[start_loc] = height[start_loc] - 1
-    height[end_loc] = height[end_loc] + 1
-    TowerOfHanoi(n-1, aux_rod, start_loc, end_loc, pub_cmd, loop_rate, height)
+# def TowerOfHanoi(n, start_loc, end_loc, aux_rod, pub_cmd, loop_rate, height):
+#     if n == 0:
+#         return
+#     TowerOfHanoi(n-1, start_loc, aux_rod, end_loc, pub_cmd, loop_rate, height)
+#     move_block(pub_cmd, loop_rate, start_loc, height[start_loc], end_loc, height[end_loc])
+#     print("Move disk", n, "from rod", start_loc, "to rod", end_loc)
+#     height[start_loc] = height[start_loc] - 1
+#     height[end_loc] = height[end_loc] + 1
+#     TowerOfHanoi(n-1, aux_rod, start_loc, end_loc, pub_cmd, loop_rate, height)
 
 
 def main():
@@ -261,7 +274,8 @@ def main():
     height = [0,0,0]
 
     while(not input_done):
-        input_string = input("Enter number of loops <Either 1 2 3 or 0 to quit> ")
+        # input_string = input("Enter number of loops <Either 1 2 3 or 0 to quit> ")
+        input_string = input("Enter the start and end location <Either 1 2 3 or 0 to quit> ")
         print("You entered " + input_string + "\n")
         res = input_string.split(" ")
         if (len(res) != 2):
@@ -270,7 +284,7 @@ def main():
         start_loc = int(res[0])
         end_loc = int(res[1])
         input_done = 1
-        if (start_loc == end_loc):
+        if (start_loc == end_loc) or int(res[0]) == 0 or int(res[1]) == 0:
             print("Quitting... ")
             sys.exit()
    
@@ -289,11 +303,13 @@ def main():
         # else:
         #     print("Please just enter the character 1 2 3 or 0 to quit \n\n")
 
-    pos = [0,1,2]
+    pos = [1, 2, 3]
     pos.remove(start_loc)
     pos.remove(end_loc)
-    aux_rod = pos[0]
-    print("aux is:" + str(aux_rod))
+    middle_loc = pos[0]
+    print("Middle Location is:" + str(middle_loc))
+    # aux_rod = pos[0]
+    # print("aux is:" + str(aux_rod))
 
 
 
@@ -310,9 +326,52 @@ def main():
     ############## Your Code Start Here ##############
     # TODO: modify the code so that UR3 can move tower accordingly from user input
 
-    while(loop_count > 0):
-        height[start_loc] = 2
-        TowerOfHanoi(3, start_loc, end_loc, aux_rod, pub_command, loop_rate, height)
+    # while(loop_count > 0):
+    #     height[start_loc] = 2
+    #     TowerOfHanoi(3, start_loc, end_loc, aux_rod, pub_command, loop_rate, height)
+
+    middle_loc = 0
+    if((start_loc == 1 and end_loc == 3) or (start_loc == 3 and end_loc == 1)) :
+        middle_loc = 2
+    if((start_loc == 1 and end_loc == 2) or (start_loc == 2 and end_loc == 1)) :
+        middle_loc = 3
+    if((start_loc == 2 and end_loc == 3) or (start_loc == 3 and end_loc == 2)) :
+        middle_loc = 1
+    block_error = 0
+    gripper(pub_command, loop_rate, suction_off)
+    move_arm(pub_command, loop_rate, home, 4.0, 4.0)
+    rospy.loginfo("Sending goal 1 ...")
+    block_error = move_block(pub_command, loop_rate, start_loc - 1, 2, end_loc - 1, 0)
+    if(block_error == 1):
+        print("Oops, there's something wrong with the block")
+        sys.exit()
+    block_error = move_block(pub_command, loop_rate, start_loc - 1, 1, middle_loc - 1, 0)
+    if(block_error == 1):
+        print("Oops, there's something wrong with the block")
+        sys.exit()
+    block_error = move_block(pub_command, loop_rate, end_loc - 1, 0, middle_loc - 1, 1)
+    if(block_error == 1):
+        print("Oops, there's something wrong with the block")
+        sys.exit()
+    block_error = move_block(pub_command, loop_rate, start_loc - 1, 0, end_loc - 1, 0)
+    if(block_error == 1):
+        print("Oops, there's something wrong with the block")
+        sys.exit()
+    block_error = move_block(pub_command, loop_rate, middle_loc - 1, 1, start_loc - 1, 0)
+    if(block_error == 1):
+        print("Oops, there's something wrong with the block")
+        sys.exit()
+    block_error = move_block(pub_command, loop_rate, middle_loc - 1, 0, end_loc - 1, 1)
+    if(block_error == 1):
+        print("Oops, there's something wrong with the block")
+        sys.exit()
+    block_error = move_block(pub_command, loop_rate, start_loc - 1, 0, end_loc - 1, 2)
+    if(block_error == 1):
+        print("Oops, there's something wrong with the block")
+        sys.exit()
+    print("Congrats! It works!")
+    move_arm(pub_command, loop_rate, home, 4.0, 4.0)
+    sys.exit()
 
 
     #     move_arm(pub_command, loop_rate, home, 4.0, 4.0)
@@ -330,7 +389,7 @@ def main():
     #     rospy.loginfo("Sending goal 3 ...")
     #     move_arm(pub_command, loop_rate, Q[2][0], 4.0, 4.0)
 
-        loop_count = loop_count - 1
+        # loop_count = loop_count - 1
 
     # gripper(pub_command, loop_rate, suction_off)
 
